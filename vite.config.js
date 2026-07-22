544e008db07cbae62654a7385f191521d7438084
Muzaffer Karsli <muzafferkarsli@gmail.com>
2026-07-22 15:42:22 +0300

    ci: add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
 
 export default defineConfig({
+  base: '/autopoiesis/',
   build: {
     chunkSizeWarningLimit: 600,
     rollupOptions: {
